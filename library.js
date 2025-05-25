78ba1318d14d89ae6c285153963f55a6a6275903
sjmy <23250749+sjmy@users.noreply.github.com>
2025-05-24 20:39:13 -0700

    Added addBookToLibrary() function

--- library.js
+++ library.js
@@ -1,3 +1,6 @@
+const myLibrary = [];
+
+// Constructor
 function Book(title, author, pages) {
     // This is a way of checking to see if the variable was declared using new.
     if (!new.target) {
@@ -8,6 +11,7 @@ function Book(title, author, pages) {
     this.author = author;
     this.pages = pages;
     this.isRead = false;
+    this.id = crypto.randomUUID();
 
     this.info = function() {
         if (this.isRead) {
@@ -18,5 +22,19 @@ function Book(title, author, pages) {
     };
 };
 
-let theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", 295);
-console.log(theHobbit.info());
+function addBookToLibrary(title, author, pages) {
+    // Is there a better way to search for array objects? myLibrary.includes()?
+    for (n = 0; n < myLibrary.length; n++) {
+        if (myLibrary[n].title == title) {
+            console.log("This book is already in the library!");
+            return;
+        };
+    };
+    
+    new_book = new Book(title, author, pages);
+    myLibrary.push(new_book);
+};
+
+addBookToLibrary("The Hobbit", "J.R.R. Tolkien", 295);
+addBookToLibrary("Somehow I Manage", "Michael Scott", 78);
+console.table(myLibrary);
