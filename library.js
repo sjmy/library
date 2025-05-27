2a04acd29f7daac3c9059054c57c900d86620afb
sjmy <23250749+sjmy@users.noreply.github.com>
2025-05-26 20:18:50 -0700

    displayBooks() and addNewBook button working

--- library.js
+++ library.js
@@ -1,7 +1,11 @@
 const myLibrary = [];
+const addNewBookDialog = document.querySelector(".addNewBook");
+const showAddNewBookDialog = document.querySelector(".addButton");
+const confirmAddNewBook = document.querySelector(".confirmButton")
+const cancelAddNewBook = document.querySelector(".cancelButton");
 
 // Constructor
-function Book(title, author, pages) {
+function Book(title, author, pages, isRead = "Unread") {
     // This is a way of checking to see if the variable was declared using new.
     if (!new.target) {
         throw Error("You must use the 'new' operator to call the constructor");
@@ -10,48 +14,92 @@ function Book(title, author, pages) {
     this.title = title;
     this.author = author;
     this.pages = pages;
-    this.isRead = false;
+    this.isRead = isRead;
     this.id = crypto.randomUUID();
-
-    this.info = function() {
-        if (this.isRead) {
-            return `${this.title} by ${this.author}, ${this.pages} pages, read.`;
-        } else {
-            return `${this.title} by ${this.author}, ${this.pages} pages, not read yet.`;
-        };
-    };
 };
 
 function addBookToLibrary(title, author, pages) {
     // Is there a better way to search for array objects? myLibrary.includes()?
     for (n = 0; n < myLibrary.length; n++) {
-        if (myLibrary[n].title == title) {
+        if (myLibrary[n].title == title && myLibrary[n].author == author) {
             console.log("This book is already in the library!");
             return;
         };
     };
     
-    const new_book = new Book(title, author, pages);
-    myLibrary.push(new_book);
+    const newBook = new Book(title, author, pages);
+    myLibrary.push(newBook);
     displayBooks();
 };
+
+// function checkID(newID) {
+//     // Check to see if the book is already in the array
+//     // Currently not working
+
+//     const tblBody = document.querySelector("tbody");
+    
+//     for (i = 0; i < tblBody.rows.length; i++) {
+//         const row = tblBody.rows[i];
+
+//         for (j = 0; j < row.cells.length; j++) {
+//             console.log(row.cells[j].textContent);
+//             console.log(newID);
+//             if (row.cells[j].textContent == newID) {
+//                 return;
+//             };
+//         };
+//     };
+// };
 
 function displayBooks() {
     const tblBody = document.querySelector("tbody");
 
-    for (i = 0; i < 2; i++) {
-        const row = document.createElement("tr");
+    // Deletes all the table rows to ensure no duplicates are displayed
+    const tblBodyRows = tblBody.rows.length;
+    for (r = 1; r < tblBodyRows; r++) {
+        tblBody.deleteRow(1);
+    };
 
-        for (j = 0; j < 5; j++) {
-            const cell = document.createElement("td");
-            const book = myLibrary[i];
-            row.appendChild(book.title);
+    // Create a new table row for each book in the library
+    for (const book in myLibrary) {
+        const newRow = tblBody.insertRow();
+
+        // Create a new cell within the new row for each book property
+        for (const property in myLibrary[book]) {
+            newRow.insertCell().textContent = myLibrary[book][property];
         };
-
-        tblBody.appendChild(row);
     };
 };
 
+
+// Event Listeners
+
+showAddNewBookDialog.addEventListener("click", () => {
+    addNewBookDialog.showModal();
+});
+
+confirmAddNewBook.addEventListener("click", (e) => {
+    const bookTitle = document.querySelector("#bookTitle").value;
+    const bookAuthor = document.querySelector("#bookAuthor").value;
+    const bookPages = document.querySelector("#bookPages").value;
+    // const bookIsRead = document.querySelector("#bookIsRead").value;
+
+    addBookToLibrary(bookTitle, bookAuthor, bookPages);
+    
+    console.log(bookTitle);
+    console.log(bookAuthor);
+    console.log(bookPages);
+    // console.log(bookIsRead);
+    e.preventDefault();
+});
+
+cancelAddNewBook.addEventListener("click", () => {
+    addNewBookDialog.close();
+});
+
+
+
 addBookToLibrary("The Hobbit", "J.R.R. Tolkien", 295);
-addBookToLibrary("Somehow I Manage", "Michael Scott", 78);
+addBookToLibrary("Somehow I Manage", "Michael Scott", 31);
+addBookToLibrary("Case Closed, Jake Open", "Jake Peralta", 99);
 console.table(myLibrary);
